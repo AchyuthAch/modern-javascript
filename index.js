4e03460cc9ac1c63ccc727eb2de9ffdb488a87e9
AchyuthAch <40026318+AchyuthAch@users.noreply.github.com>
2025-04-21 17:52:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// Let & Const are the variables used to store the data
+//Let can be overidden and const cannot be overidden
+
+// String Methods Examples
+
+//Concatenation
+let firstName = "Achyuth"
+let secondName = "S"
+let fullName = firstName + " " + secondName
+console.log(fullName)
+//Output would be Achyuth S
+
+//To find the string length
+let len = firstName.length
+console.log(len)
+//Outout would be 7
+
+//Indexof Method
+let email = "achyuth.333@gmail.com"
+let check = email.indexOf("@")
+console.log(check)
+//Output would be 11
+
+// Upper & Lower case methods
+
+let upper = firstName.toUpperCase()
+let lower = secondName.toLowerCase()
+console.log(upper, lower)
+//Output would be ACHYUTH s
