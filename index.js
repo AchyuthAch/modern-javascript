d102e6188cbc784534663e70c15f42c33d79c159
AchyuthAch <40026318+AchyuthAch@users.noreply.github.com>
2025-04-22 14:14:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,48 @@ let upper = firstName.toUpperCase()
 let lower = secondName.toLowerCase()
 console.log(upper, lower)
 //Output would be ACHYUTH s
+
+            // 0123456789
+let myemail = "achyuth.333@gmail.com.in";
+let result = myemail.lastIndexOf("@")
+console.log(result)
+// Output would be 11
+
+let sliceexample = myemail.slice(2,5)
+console.log("sliceexample", sliceexample)
+//output would be hyu
+
+let substrng = myemail.substring(2,5)
+console.log("substrng", substrng)
+//output would be hyu
+
+let replacesample = myemail.replace("a", "r")
+console.log("replacesample", replacesample)
+
+////////////////////////Numbers////////////////////////////////
+
+let likes = 10
+
+//Order of calculations done at JS would be B I D M A S
+// likes++
+// console.log("likes", likes)
+// //Output would be 11
+
+likes += 10
+console.log("finallikes", likes)
+//Output would be 20
+
+likes -= 10
+likes /= 2
+///////////////Nan(Not a number)////////////////
+console.log(5 / "hello")
+
+
+///////////////Template String////////////
+let count_likes = 10
+const author = "Achyuth"
+const title = "Na Istam"
+
+let finalJudgement = `Hello guys I'm ${author} and with name of the book as ${title} with ${count_likes}`
+console.log("finalJudgement", finalJudgement)
+
