e19eb5eb746ee221384f8bbb4b9102bb037421a8
AchyuthAch <40026318+AchyuthAch@users.noreply.github.com>
2025-04-23 23:24:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,3 +72,78 @@ const title = "Na Istam"
 let finalJudgement = `Hello guys I'm ${author} and with name of the book as ${title} with ${count_likes}`
 console.log("finalJudgement", finalJudgement)
 
+
+/////////////////Arrays///////////////////
+
+let people = ["Achyuth", "Achu", "Achuuu"]
+console.log("people", people)
+console.log("get1", people[1]);
+people[1] = "Mario"
+console.log("againpeople", people)
+//Length property of array
+console.log(people.length)
+
+//Array Methods////
+//Join method
+let join_all = people.join(",")
+console.log(join_all)
+//Indexof method at array
+let find_index = people.indexOf("Achuuu")
+console.log("find_index", find_index)
+//concat method at arrays
+let concat_two = people.concat(['ninja', 'cool'])
+console.log("after concat", concat_two)
+//push & pop methods at arrays
+let push_one = people.push("push1")
+console.log("push_one", push_one)
+console.log("people_pushed", people)
+let pop_one = people.pop("push1")
+console.log("pop_people", people)
+
+/////////NULL & UNDEFINED////////////
+let age = null;
+
+console.log(age, age + 3, `this is my ${age}`)
+
+////////////////Booleans//////////////////
+let names = ['Achyuth', "Mario", "ninja"]
+
+let check1 = names.includes("Achyuth")
+console.log(check1)
+
+let age1 = 25;
+console.log(age1 == 25)
+console.log(age1 != 22)
+
+let checkk111 = "sachyuth"
+console.log(checkk111 != "achyuth")
+
+/// Capital letter would be higer than lower letter
+console.log(checkk111 > "achyuth")
+
+//Here s would be first and c would be second, So it would throw true
+console.log(checkk111 > "cachyuth")
+
+///Loose operators///////
+let op1 = 25
+console.log(op1 == 25)
+console.log(op1 == '25')
+//Output would be true here as JS things both are same types(Number & Strings)
+console.log(op1 != 25)
+console.log(op1 != '25')
+//Output would be false here as JS things both are same types(Number & Strings)
+
+////Strict Operators/////////
+let strict11 = 255
+console.log(strict11 === 255) //true
+console.log(strict11 === '255') //false due to different types
+
+//Type Conversion//
+let score = 100;
+finalScore = Number(score)
+console.log(finalScore+1)
+console.log(typeof score)
+
+//Boolean conversions///
+let bool = 12
+console.log(bool, Boolean(''))
