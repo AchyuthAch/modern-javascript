7f153af528247d8c1fefc2c6e26f7a899dda5313
AchyuthAch <40026318+AchyuthAch@users.noreply.github.com>
2025-04-24 00:22:50 +0530

    Create control_flow.js

---
+++ control_flow.js
@@ -0,0 +1,32 @@
+//For Loops examples
+//Example 1:
+for(i = 0; i<5 ; i++){
+    console.log("Inside Loop", i)
+}
+console.log("loop finished")
+
+//Example 2:
+const names = ["Achyuth", "Mario", "Ninja"];
+for(i = 0; i<names.length; i++){
+    console.log(names[i])
+}
+
+//While Loop examples
+//Example1:
+let a = 0;
+while(a<5){
+    console.log("Inside while loop", a);
+    a++;
+}
+
+//Example2:
+b = 0;
+const whileNames = ["Achyuth", "Mario", "Ninja"]
+while(b < whileNames.length){
+    console.log(whileNames[b])
+    b++;
+}
+
+//Do While Loops
+//Example 1:
+
