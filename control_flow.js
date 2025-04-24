8d4681129bb5215d44358f9589db37667f679975
AchyuthAch <40026318+AchyuthAch@users.noreply.github.com>
2025-04-25 00:13:59 +0530

    Update control_flow.js

--- control_flow.js
+++ control_flow.js
@@ -11,6 +11,7 @@ for(i = 0; i<names.length; i++){
     console.log(names[i])
 }
 
+
 //While Loop examples
 //Example1:
 let a = 0;
@@ -29,4 +30,64 @@ while(b < whileNames.length){
 
 //Do While Loops
 //Example 1:
+const doWhileNames = ["Dowhile", "Mario", "Ninja", "Achyuth", "Achu", "DODO"]
+console.log("len", doWhileNames.length)
+c = 5;
+do{
+    console.log(doWhileNames[c])
+    c++;
+    }
+while(c < doWhileNames.length)
+
+//Example 2:
+let d = 4;
+
+do{
+    console.log("Inside of dowhile ", d)
+    d++
+}while (d<3);
+
+//If Statements
+//Example 1:
+const forAge = 23;
+if(forAge>30) {
+    console.log("Your are uncle")
+}else
+console.log("You are still not uncle.. Enjoyyy")
+
+//Example 2:
+const password = "Hah114"
+
+if (password.length>= 12){
+    console.log("You have entered strong password")
+}
+else if(password.length>5){
+    console.log("You have entered valid password")
+}else{
+    console.log("Your password must be atleast 5 chars")
+}
+
+//Logical Operators || and &&
+
+//Example 1:
+const pswd = "Pass@qqi1243"
+if(pswd.length>10 && pswd.includes('@')){
+    console.log("STRONG")
+}else if(pswd.length>5 || pswd.includes("@")){
+    console.log("MEDIUM")
+}else{
+    console.log("POOR & Type again for STRONG & MEDIUM")
+}
+
+//Logical NOT (!)
+//Example 1:
+
+console.log(!true)
+console.log(!false)
+
+let user = false;
+//Until true if block will never executes
+if(!user){
+    console.log("Logged in")
+}
 
